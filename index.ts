547b96dcfc39e4c93b281799f2b02d06a233d5f6
yaroslav8765 <pechorkin2014@gmail.com>
2026-06-24 14:07:53 +0300

    refactor: improve key handling and logging in ResourceKeyValueAdapter
    AdminForth/1735/kv-adapters-and-storage-s3-com

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import type { KeyValueAdapter, IAdminForth } from "adminforth";
-import { Filters, Sorts } from 'adminforth';
+import { Filters, Sorts, afLogger } from 'adminforth';
 import type { AdapterOptions } from "./types.js";
 
-export default class RAMKeyValueAdapter implements KeyValueAdapter {
+export default class ResourceKeyValueAdapter implements KeyValueAdapter {
   adminForth: IAdminForth | undefined;
   options: AdapterOptions;
 
@@ -22,39 +22,52 @@ export default class RAMKeyValueAdapter implements KeyValueAdapter {
     return this.adminForth!.resource(this.options.resourceId);
   }
 
+  protected getActualKey(key: string, collection?: string): string {
+    if (collection) {
+      return `${collection}:${key}`;
+    }
+    return key;
+  }
+
   async get(key: string, collection?: string): Promise<string> {
     const resource = this.getResource();
-    if (collection) {
-      return await resource.get(Filters.AND(Filters.EQ(this.options.collectionField, collection), Filters.EQ(this.options.keyField, key)));
+    const actualKey = this.getActualKey(key, collection);
+    const record = await resource.get(Filters.EQ(this.options.keyField, actualKey));
+    if (record) {
+      return record[this.options.valueField];
     } else {
-      return await resource.get(Filters.EQ(this.options.keyField, key));
+      console.error(`ResourceKeyValueAdapter: Key not found: ${actualKey} in collection: ${collection}`);
     }
   }
 
   async set(key: string, value: any, expiresInSeconds?: number, collection?: string): Promise<void> {
     const resource = this.getResource();
+    if (expiresInSeconds) {
+      afLogger.error(`ResourceKeyValueAdapter does not support expiresInSeconds yet. Ignoring expiresInSeconds for key: ${key} and collection: ${collection}`);
+    }
+    const actualKey = this.getActualKey(key, collection);
     if (collection) {
-      const existing = await resource.get(Filters.AND(Filters.EQ(this.options.collectionField, collection), Filters.EQ(this.options.keyField, key)));
+      const existing = await resource.get(Filters.AND(Filters.EQ(this.options.collectionField, collection), Filters.EQ(this.options.keyField, actualKey)));
       if (existing) {
-        await resource.update(Filters.AND(Filters.EQ(this.options.collectionField, collection), Filters.EQ(this.options.keyField, key)), {
+        await resource.update(Filters.AND(Filters.EQ(this.options.collectionField, collection), Filters.EQ(this.options.keyField, actualKey)), {
           [this.options.valueField]: value,
         });
       } else {
         await resource.create({
-          [this.options.keyField]: key,
+          [this.options.keyField]: actualKey,
           [this.options.valueField]: value,
           [this.options.collectionField]: collection,
         });
       }
     } else {
-      const existing = await resource.get(Filters.EQ(this.options.keyField, key));
+      const existing = await resource.get(Filters.EQ(this.options.keyField, actualKey));
       if (existing) {
-        await resource.update(Filters.EQ(this.options.keyField, key), {
+        await resource.update(Filters.EQ(this.options.keyField, actualKey), {
           [this.options.valueField]: value,
         });
       } else {
         await resource.create({
-          [this.options.keyField]: key,
+          [this.options.keyField]: actualKey,
           [this.options.valueField]: value,
         });
       }
@@ -63,19 +76,21 @@ export default class RAMKeyValueAdapter implements KeyValueAdapter {
 
   async delete(key: string, collection?: string): Promise<void> {
     const resource = this.getResource();
-    if (collection) {
-      await resource.delete(Filters.AND(Filters.EQ(this.options.collectionField, collection), Filters.EQ(this.options.keyField, key)));
-    } else {
-      await resource.delete(Filters.EQ(this.options.keyField, key));
-    }
+    const actualKey = this.getActualKey(key, collection);
+    await resource.delete(actualKey);
   }
 
   async listByPrefix(prefix: string, limit?: number, collection?: string): Promise<Record<string, string>[]> {
     const resource = this.getResource();
-    if (collection) {
-      return await resource.list(Filters.AND(Filters.EQ(this.options.collectionField, collection), Filters.LIKE(this.options.keyField, `${prefix}%`)), limit, 0, Sorts.ASC(this.options.keyField));
-    } else {
-      return await resource.list(Filters.LIKE(this.options.keyField, `${prefix}%`), limit, 0, Sorts.ASC(this.options.keyField));
-    }
+    const actualPrefix = this.getActualKey(prefix, collection);
+    const list = await resource.list(Filters.LIKE(this.options.keyField, `${actualPrefix}%`), limit, 0, Sorts.ASC(this.options.keyField));
+
+    const keyValuePairs: Record<string, string>[] = list.map((record) => {
+      const key = record[this.options.keyField];
+      const value = record[this.options.valueField];
+      return { [key]: value };
+    });
+
+    return keyValuePairs;
   }
 }
